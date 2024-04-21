6a88497302996b9aa69f568eea72cf86c4b25f53
Jorsch-0 <jorge.leon@jortech.cc>
2024-04-21 00:30:15 -0500

    refactor

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:3001', {
+      const socket = io('https://xmas-chat.onrender.com', {
         query: {
           userId: authUser._id,
         },
